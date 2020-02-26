baf0d089f978a6af6ff04edf7bdb8b979b26a3fb
Eder Oliveira <ederco2@gmail.com>
2020-02-26 17:55:41 -0300

    Lambda Function

---
+++ Lambda3_Function/src/Util/UpperCaseName.java
@@ -0,0 +1,17 @@
+package Util;
+
+import java.util.function.Function;
+
+import entities.Product;
+
+public class UpperCaseName implements Function<Product, String>{
+
+	@Override
+	public String apply(Product p) {
+		
+		return p.getName().toUpperCase();
+	}
+
+
+
+}
